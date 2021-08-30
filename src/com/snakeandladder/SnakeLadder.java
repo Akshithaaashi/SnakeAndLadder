5aad3e90a48d1027d872dee0f722fe128db99ec5
Akshithaaashi <akshitha.aashi@gmail.com>
2021-08-30 13:53:38 +0530

    Added : roll to dice

--- src/com/snakeandladder/SnakeLadder.java
+++ src/com/snakeandladder/SnakeLadder.java
@@ -1,15 +1,22 @@
 package com.snakeandladder;
 import java.util.Scanner;
+import java.util.Random;
 
-public class SinglePlayer {
-
+public class SnakeLadder 
+{	
 	public static void main(String[] args) 
 	{
 	      System.out.println("Welcome to SnakeAndLadder program");
               int position=0;
 	       System.out.println("Player is at position : "+position);
 		
-
-}
-
+		Random random = new Random();
+	    int droll = 0;
+		while (true)
+		{
+    		droll = random.nextInt(7);
+    		if(droll !=0) break;
+		}
+		System.out.println(droll);
+	}
 }
