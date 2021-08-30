ff6482e0d11e2fb36af10994e0e3908f2c197c1f
Akshithaaashi <akshitha.aashi@gmail.com>
2021-08-30 13:40:50 +0530

    Added : single player at position 0

---
+++ src/com/snakeandladder/SinglePlayer.java
@@ -0,0 +1,15 @@
+package com.snakeandladder;
+import java.util.Scanner;
+
+public class SinglePlayer {
+
+	public static void main(String[] args) 
+	{
+	      System.out.println("Welcome to SnakeAndLadder program");
+              int position=0;
+	       System.out.println("Player is at position : "+position);
+		
+
+}
+
+}
