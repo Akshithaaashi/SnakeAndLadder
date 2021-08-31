73b5325baafc551461684c01af3cda13fae26a74
Akshithaaashi <akshitha.aashi@gmail.com>
2021-08-31 15:28:58 +0530

    Added : number of times the dice was played

--- src/com/snakeandladder/SnakeLadder.java
+++ src/com/snakeandladder/SnakeLadder.java
@@ -2,72 +2,81 @@ package com.snakeandladder;
 import java.util.Scanner;
 import java.util.Random;
 
+
 public class SnakeLadder 
 {	
 
-        public static final int No_Play=1;
-        public static final int Ladder=2;
-        public static final int Snake=3;
-        public static final int Win_position=100;
-        public static final int Position_check = 0;
+  public static final int No_Play=1;
+  public static final int Ladder=2;
+  public static final int Snake=3;
+  public static final int Win_position=100;
+  public static final int Position_check = 0;
 
 
-	public static void main(String[] args) 
+public static void main(String[] args) 
+{
+    System.out.println("Welcome to SnakeAndLadder program");
+        int noOfDieRoll = 0;
+        int position=0;
+     System.out.println("Start position of player : "+position);
+	
+
+          while (position < Win_position){
+//Rolling dice to get random number
+	Random random = new Random();
+  int droll = 0;
+	while (true)
 	{
-	      System.out.println("Welcome to SnakeAndLadder program");
-              int position=0;
-	       System.out.println("Start position of player : "+position);
-	        System.out.println("Start position of player :	"+position);
+		droll = random.nextInt(7);
+		if(droll !=0) break;
+	}
 
+	System.out.println("Number of dice : "+droll);
 
-		
-		Random random = new Random();
-	    int droll = 0;
-		while (true)
-		{
-    		droll = random.nextInt(7);
-    		if(droll !=0) break;
-		}
-		System.out.println(droll);
-		System.out.println("Number of dice : "+droll);
+	//o
+          Random ran = new Random();
+          noOfDieRoll++;        
+          int option = 0;
+	while (true)
+	{
+		option = ran.nextInt(4);
+		if(option !=0) break;
+	}
+	System.out.println("====OPTIONS======\nOption 1 : No Play\nOption 2 : Ladder \nOption 3 : Snake \n");
+	System.out.println("Option : "+option);
 
-		//options
+	switch (option) {
+		case No_Play:
+			System.out.println("Player stays in the same position : "+position);
+			break;
 
-		Random ran = new Random();
-	    int option = 0;
-		while (true)
-		{
-    		option = ran.nextInt(4);
-    		if(option !=0) break;
-		}
-		System.out.println("====OPTIONS======\nOption 1 : No Play\nOption 2 : Ladder \nOption 3 : Snake \n");
-		System.out.println("Option : "+option);
+		case Ladder:
+                          position += droll;
+                          if ((position + droll) > Win_position){
+                                  position = Win_position;
+                          } else{
+                                  position += droll;
+                          }
+			
+			System.out.println("Player moves ahead by : "+position);
+			break;
 
-		switch (option) {
-			case No_Play:
-				System.out.println("Player stays in the same position : "+position);
-				break;
-
-			case Ladder:
-				position += droll;
-				System.out.println("Player moves ahead by : "+position);
-				break;
-
-			case Snake:
-				position -= droll;
-                position -= droll;
-                if ((position - droll) < Position_check){
-                	position = Position_check;
+		case Snake:
+			position -= droll;
+          position -= droll;
+          if ((position - droll) < Position_check){
+          	position = Position_check;
 }
-              System.out.println("Player moves behind by : "+position);
-              break;			
+        System.out.println("Player moves behind by : "+position);
+        break;			
 
-			default: 
-				System.out.println("Enter correct value");
+		default: 
+			System.out.println("Enter correct value");
 
 break;
+	}
+System.out.println("Number Of Times The Dice Was Played To Win The Game Is : " +noOfDieRoll);
 
-              }
-	  }
-
-    }
+            }
+          }
+}
